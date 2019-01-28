bbdb9624fd9b871182b2f46b0e61e406b47eb370
Bret Gourdie <bretgourdie@gmail.com>
2019-01-28 17:23:39 -0500

    Added GravitySystem

---
+++ NezzyBird/Systems/GravitySystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nez;
+using NezzyBird.Components;
+
+namespace NezzyBird.Systems
+{
+    public class GravitySystem : EntityProcessingSystem
+    {
+        public GravitySystem(Matcher matcher) : base(matcher) { }
+
+        public override void process(Entity entity)
+        {
+            var gravity = entity.getComponent<AffectedByGravity>();
+        }
+    }
+}
