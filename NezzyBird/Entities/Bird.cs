d11ff36e52197d992a9334d3f202331198ae054a
Bret Gourdie <bretgourdie@gmail.com>
2019-01-30 15:10:37 -0500

    Only use one sprite

--- NezzyBird/Entities/Bird.cs
+++ NezzyBird/Entities/Bird.cs
@@ -11,15 +11,30 @@ namespace NezzyBird.Entities
     {
         public Bird(TextureAtlas textureAtlas)
         {
-            var birdAnimation = textureAtlas.getSpriteAnimation("BirdAnimation");
-            var birdIdleSubtexture = textureAtlas.getSubtexture("birdflap1_2");
-            addComponent(new Sprite(birdIdleSubtexture.texture2D));
+            var lastSpriteName = textureAtlas.regionNames.lastItem();
+            var birdIdleSprite = textureAtlas.getSubtexture(lastSpriteName);
+            addComponent(new Sprite(birdIdleSprite));
             addComponent(new AffectedByGravity());
             addComponent(new JumpsOnTap());
 
             this.scale = new Vector2(3, 3);
 
             this.position = new Vector2(200, 200);
+        }
+
+        public Vector2 getBirdStart()
+        {
+            return new Vector2(150, 263);
+        }
+
+        public Vector2 getBirdSize()
+        {
+            return new Vector2(17, 12);
+        }
+
+        public Vector2 getBirdPadding()
+        {
+            return new Vector2(1, 1);
         }
     }
 }
