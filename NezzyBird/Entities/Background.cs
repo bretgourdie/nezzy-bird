ecbf28908ae6461770e93b542b48ce0a02d1ab5d
Bret Gourdie <bretgourdie@gmail.com>
2019-01-31 14:04:24 -0500

    Add Background entity

---
+++ NezzyBird/Entities/Background.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.Sprites;
+using Nez.TextureAtlases;
+
+namespace NezzyBird.Entities
+{
+    public class Background : Entity
+    {
+        public Background(TextureAtlas atlas)
+        {
+            var selectedBackground = Random.choose("Day", "Night");
+            var backgroundSubtexture = atlas.getSubtexture(selectedBackground);
+            var backgroundSprite = new Sprite(backgroundSubtexture);
+            backgroundSprite.renderLayer = 100;
+            addComponent(backgroundSprite);
+
+            this.scale = GameConstants.GetGameScale();
+
+            var middleOfScreen = new Vector2(
+                backgroundSprite.width / 2,
+                backgroundSprite.height / 2);
+            this.setPosition(middleOfScreen);
+        }
+    }
+}
