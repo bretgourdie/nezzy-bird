0b6d5fcbb3a343f242ad5aafba43a49e379d0e81
Bret Gourdie <BretGourdie@gmail.com>
2019-02-01 19:54:21 -0500

    Cleanup and use correct Content

--- NezzyBird/Game1.cs
+++ NezzyBird/Game1.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.Xna.Framework;
-using Nez;
+﻿using Nez;
 using NezzyBird.Scenes;
 
 namespace NezzyBird
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Nez.Core
     {
         public Game1() : base(
@@ -14,17 +10,11 @@ namespace NezzyBird
             height: (int)GameConstants.GetScreenBounds().Y,
             windowTitle: GameConstants.GAME_NAME) { }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             base.Initialize();
 
-            var mainScene = new MainScene(content);
+            var mainScene = new MainScene(Content);
 
             Core.scene = mainScene;
         }
