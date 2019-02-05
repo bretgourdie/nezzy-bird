71a01e2147012f21677a7bfd0061e38a9f2ece96
Bret Gourdie <BretGourdie@gmail.com>
2019-02-04 23:14:54 -0500

    Wired up virtual button for jumping

--- NezzyBird/Components/JumpsOnTap.cs
+++ NezzyBird/Components/JumpsOnTap.cs
@@ -1,4 +1,5 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework.Input;
+using Nez;
 
 namespace NezzyBird.Components
 {
@@ -6,14 +7,31 @@ namespace NezzyBird.Components
     {
         private readonly float _jumpAmount;
 
+        private VirtualButton _jumpButton;
+
         public JumpsOnTap(float jumpAmount)
         {
             _jumpAmount = jumpAmount;
         }
 
+        public override void onAddedToEntity()
+        {
+            _jumpButton = new VirtualButton();
+            _jumpButton.addKeyboardKey(Keys.Space);
+            _jumpButton.addMouseLeftButton();
+        }
+
+        public override void onRemovedFromEntity()
+        {
+            _jumpButton.deregister();
+        }
+
         public void update()
         {
-            throw new System.NotImplementedException();
+            if (_jumpButton.isPressed)
+            {
+                Nez.Debug.log("you just jumped!");
+            }
         }
     }
 }
