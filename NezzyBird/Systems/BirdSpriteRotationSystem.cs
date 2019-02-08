5c6ee01175ffd26e808956e277ef9cee374f27dd
Bret Gourdie <BretGourdie@gmail.com>
2019-02-07 23:53:32 -0500

    Use velocity-based sprite rotation

--- NezzyBird/Systems/BirdSpriteRotationSystem.cs
+++ NezzyBird/Systems/BirdSpriteRotationSystem.cs
@@ -1,31 +1,35 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Nez;
 using Nez.Sprites;
 using NezzyBird.Components;
+using NezzyBird.Entities;
+using System;
 
 namespace NezzyBird.Systems
 {
     public class BirdSpriteRotationSystem : EntityProcessingSystem
     {
-        public BirdSpriteRotationSystem() : base(
-            new Matcher().all(
-                typeof(JumpsOnTap),
-                typeof(Sprite)
-            ))
-        {
-        }
+        public BirdSpriteRotationSystem() : base(new Matcher().all(typeof(HasVelocity), typeof(Sprite))) { }
 
         public override void process(Entity entity)
         {
-            var jumpsOnTap = entity.getComponent<JumpsOnTap>();
+            var velocity = entity.getComponent<HasVelocity>();
             var sprite = entity.getComponent<Sprite>();
 
-            if (jumpsOnTap.IsJumping)
-            {
-                sprite.transform.tweenRotationDegreesTo(-40f)
-                    .setNextTween(
-                        sprite.transform.tweenRotationDegreesTo(90f, 0.5f))
-                .start();
-            }
+            var lowerBound = -2f;
+            var upperBound = Bird.JUMP_HEIGHT;
+
+            var clampedVelocity =
+                MathHelper.Clamp(
+                    velocity.CurrentVelocity.Y,
+                    lowerBound,
+                    upperBound);
+
+            var rotationSignificance = (clampedVelocity - lowerBound) / (Math.Abs(upperBound - lowerBound));
+
+            var rotationDegrees = MathHelper.Lerp(-40, 90, rotationSignificance);
+
+            sprite.transform.rotationDegrees = rotationDegrees;
         }
     }
 }
