ca692a7a399c06f061bbb69aefd92372489d4197
Bret Gourdie <BretGourdie@gmail.com>
2019-02-14 15:25:14 -0500

    Implement scrolling

--- NezzyBird/Components/Scrolling.cs
+++ NezzyBird/Components/Scrolling.cs
@@ -1,11 +1,11 @@
-﻿using Nez;
+﻿using System;
+using Microsoft.Xna.Framework;
+using Nez;
 
 namespace NezzyBird.Components
 {
     public class Scrolling : Component
     {
-        private readonly float _rate;
-
         public enum ScrollDirection
         {
             Up,
@@ -14,12 +14,30 @@ namespace NezzyBird.Components
             Right
         }
 
-        private readonly ScrollDirection _scrollDirection;
+        public readonly Vector2 Movement;
 
         public Scrolling(ScrollDirection scrollDirection, float rate)
         {
-            _scrollDirection = scrollDirection;
-            _rate = rate;
+            Movement = determineMovement(scrollDirection, rate);
+        }
+
+        private Vector2 determineMovement(
+            ScrollDirection scrollDirection,
+            float rate)
+        {
+            switch (scrollDirection)
+            {
+                case ScrollDirection.Up:
+                    return new Vector2(0, -rate);
+                case ScrollDirection.Left:
+                    return new Vector2(-rate, 0);
+                case ScrollDirection.Down:
+                    return new Vector2(0, rate);
+                case ScrollDirection.Right:
+                    return new Vector2(rate, 0);
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
