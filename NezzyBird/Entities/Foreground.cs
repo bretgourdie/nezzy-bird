74dfcada2debf8ef7f615828d3b08edb6bf16f9d
Bret Gourdie <BretGourdie@gmail.com>
2019-02-19 14:36:09 -0500

    Add Foreground entity

---
+++ NezzyBird/Entities/Foreground.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.Sprites;
+using Nez.TextureAtlases;
+
+namespace NezzyBird.Entities
+{
+    public class Foreground : Entity
+    {
+
+        public Foreground(TextureAtlas textureAtlas)
+        {
+            var foregroundSubtexture = textureAtlas.getSubtexture("Foreground");
+            addComponent(new Sprite(foregroundSubtexture));
+
+            this.scale = GameConstants.GetGameScale();
+            this.position = new Vector2(0, GameConstants.SOURCE_SCREEN_WIDTH);
+        }
+    }
+}
