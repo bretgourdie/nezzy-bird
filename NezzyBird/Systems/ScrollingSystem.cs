2709804d41f6412d7bd90aaf4be94af51213d0ae
Bret Gourdie <BretGourdie@gmail.com>
2019-02-19 23:06:18 -0500

    Initial add of ScrollingSystem

---
+++ NezzyBird/Systems/ScrollingSystem.cs
@@ -0,0 +1,19 @@
+﻿using Nez;
+using NezzyBird.Components;
+
+namespace NezzyBird.Systems
+{
+    public class ScrollingSystem : EntityProcessingSystem
+    {
+        public ScrollingSystem() :
+            base(new Matcher().all(
+                typeof(Scrolling)
+            ))
+        { }
+
+        public override void process(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
