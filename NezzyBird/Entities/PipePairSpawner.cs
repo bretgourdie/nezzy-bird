8c52b72dc261fdacd3ad182da237862fe7a314e1
Bret Gourdie <BretGourdie@gmail.com>
2019-02-28 23:56:59 -0500

    Started to add PipePairSpawner

---
+++ NezzyBird/Entities/PipePairSpawner.cs
@@ -0,0 +1,22 @@
+﻿using Nez;
+using Nez.TextureAtlases;
+
+namespace NezzyBird.Entities
+{
+    public class PipePairSpawner : Entity
+    {
+        private readonly TextureAtlas _textureAtlas;
+        private readonly Scene _scene;
+
+        public PipePairSpawner(
+            TextureAtlas textureAtlas)
+        {
+            _textureAtlas = textureAtlas;
+        }
+
+        public void SpawnPipePair()
+        {
+            scene.addEntity(new PipePair(_textureAtlas));
+        }
+    }
+}
