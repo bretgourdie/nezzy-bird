02da0c46dd49198379f5afa85f9061e6a2f57321
Bret Gourdie <BretGourdie@gmail.com>
2019-03-17 00:28:05 -0400

    Kind of adding Prallax Scrolling

---
+++ NezzyBird/Components/ParallaxScrolling.cs
@@ -0,0 +1,27 @@
+﻿using Nez;
+using Nez.Sprites;
+
+namespace NezzyBird.Components
+{
+    public class ParallaxScrolling : Component
+    {
+        public Sprite Sprite { get; private set; }
+        public Sprite SecondSprite { get; private set; }
+        private ScrollDirection _scrollDirection;
+        private float _rate;
+
+        public ParallaxScrolling(
+            ScrollDirection scrollDirection,
+            float rate)
+        {
+            _scrollDirection = scrollDirection;
+            _rate = rate;
+        }
+
+        public override void onAddedToEntity()
+        {
+            Sprite = entity.getComponent<Sprite>();
+            SecondSprite = new Sprite(Sprite.subtexture);
+        }
+    }
+}
---
+++ NezzyBird/Systems/ParallaxScrollingSystem.cs
@@ -0,0 +1,31 @@
+﻿using Nez;
+using Nez.Sprites;
+using NezzyBird.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NezzyBird.Systems
+{
+    public class ParallaxScrollingSystem : EntityProcessingSystem
+    {
+
+        public ParallaxScrollingSystem() :
+            base(new Matcher().all(
+                typeof(ParallaxScrolling),
+                typeof(Mover),
+                typeof(Sprite)
+            ))
+        { }
+
+        public override void process(Entity entity)
+        {
+            var parallaxScrolling = entity.getComponent<ParallaxScrolling>();
+            var mover = entity.getComponent<Mover>();
+
+
+        }
+    }
+}
