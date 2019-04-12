fe5fefe53695c93a3da8945ed105aac1ca1983a0
Bret Gourdie <BretGourdie@gmail.com>
2019-04-12 13:43:07 -0400

    First cut of ScoreZoneCollisionSystem

---
+++ NezzyBird/Systems/ScoreZoneCollisionSystem.cs
@@ -0,0 +1,34 @@
+﻿using Nez;
+using NezzyBird.Components;
+
+namespace NezzyBird.Systems
+{
+    public class ScoreZoneCollisionSystem : EntityProcessingSystem
+    {
+        public ScoreZoneCollisionSystem() : base(
+            new Matcher().all(
+                typeof(IncreasesScoreWhenPassing),
+                typeof(BoxCollider)
+            ))
+        { }
+
+        public override void process(Entity entity)
+        {
+            var increasesScoreWhenPassing = entity.getComponent<IncreasesScoreWhenPassing>();
+            var boxCollider = entity.getComponent<BoxCollider>();
+
+            var colliders = Physics.boxcastBroadphase(
+                entity.getComponent<Collider>().bounds);
+
+            foreach (var collider in colliders)
+            {
+                CollisionResult collisionResult;
+                if (entity.getComponent<Collider>().collidesWith(collider, out collisionResult))
+                {
+                    // Do something constructive
+                    entity.destroy();
+                }
+            }
+        }
+    }
+}
