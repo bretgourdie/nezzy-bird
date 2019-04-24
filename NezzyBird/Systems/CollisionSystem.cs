0b3f4d1f6b6bbc53b165f93fd31d0611b532b22b
Bret Gourdie <BretGourdie@gmail.com>
2019-04-24 13:53:26 -0400

    Beginning to add [Death]CollisionSystem

---
+++ NezzyBird/Systems/CollisionSystem.cs
@@ -0,0 +1,14 @@
+﻿using Nez;
+
+namespace NezzyBird.Systems
+{
+    public class CollisionSystem : EntityProcessingSystem
+    {
+        public CollisionSystem() : base(Matcher.empty()) { }
+
+        public override void process(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ NezzyBird/Systems/DeathCollisionSystem.cs
@@ -0,0 +1,27 @@
+﻿using Nez;
+using Nez.Systems;
+using NezzyBird.Components;
+
+namespace NezzyBird.Systems
+{
+    public class DeathCollisionSystem : EntityProcessingSystem
+    {
+        private readonly Emitter<NezzyEvents> _emitter;
+
+        public DeathCollisionSystem(Emitter<NezzyEvents> emitter) :
+            base(new Matcher()
+                .all(
+                    typeof(EndsGameOnCollision),
+                    typeof(BoxCollider)
+                )
+            )
+        {
+            _emitter = emitter;
+        }
+
+        public override void process(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
