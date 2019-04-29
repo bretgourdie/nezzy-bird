349673a47aeb85ba9608b98a3e0d1f619bc25551
Bret Gourdie <BretGourdie@gmail.com>
2019-04-29 13:07:36 -0400

    Implementing a little of Start Button tap

---
+++ NezzyBird/Components/ChangeSceneOnTap.cs
@@ -0,0 +1,22 @@
+﻿using Nez;
+using System;
+
+namespace NezzyBird.Components
+{
+    public class ChangeSceneOnTap : Component
+    {
+        public bool IsRequestingSceneChange { get; private set; }
+
+        private readonly Scene _transitionToScene;
+
+        public ChangeSceneOnTap(Scene transitionToScene)
+        {
+            _transitionToScene = transitionToScene;
+        }
+
+        public void RequestSceneChange(bool request)
+        {
+            IsRequestingSceneChange = request;
+        }
+    }
+}
--- NezzyBird/Entities/StartButton.cs
+++ NezzyBird/Entities/StartButton.cs
@@ -1,12 +1,25 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework.Content;
+using Nez;
 using Nez.TextureAtlases;
+using NezzyBird.Components;
+using NezzyBird.Scenes;
 
 namespace NezzyBird.Entities
 {
     public class StartButton : Entity
     {
-        public StartButton(TextureAtlas textureAtlas)
+        public StartButton(
+            TextureAtlas textureAtlas,
+            ContentManager contentManager)
         {
+            var changeSceneOnTap = new ChangeSceneOnTap(new MainScene(contentManager));
+
+            addComponent(
+                new ReactsToTap(
+                    changeSceneOnTap,
+                    changeSceneOnTap.RequestSceneChange
+                )
+            );
         }
     }
 }
