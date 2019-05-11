d58f806bb177d4897fc3a038a22e5d2d58b1eb87
Bret Gourdie <BretGourdie@gmail.com>
2019-05-10 21:31:40 -0400

    Move movement calculation to system

--- NezzyBird/Components/Scrolling.cs
+++ NezzyBird/Components/Scrolling.cs
@@ -1,35 +1,16 @@
-﻿using System;
-using Microsoft.Xna.Framework;
-using Nez;
+﻿using Nez;
 
 namespace NezzyBird.Components
 {
     public class Scrolling : Component
     {
-        public readonly Vector2 Movement;
+        public readonly ScrollDirection ScrollDirection;
+        public readonly float Rate;
 
         public Scrolling(ScrollDirection scrollDirection, float rate)
         {
-            Movement = determineMovement(scrollDirection, rate);
-        }
-
-        private Vector2 determineMovement(
-            ScrollDirection scrollDirection,
-            float rate)
-        {
-            switch (scrollDirection)
-            {
-                case ScrollDirection.Up:
-                    return new Vector2(0, -rate);
-                case ScrollDirection.Left:
-                    return new Vector2(-rate, 0);
-                case ScrollDirection.Down:
-                    return new Vector2(0, rate);
-                case ScrollDirection.Right:
-                    return new Vector2(rate, 0);
-                default:
-                    throw new NotImplementedException();
-            }
+            ScrollDirection = scrollDirection;
+            Rate = rate;
         }
     }
 }
--- NezzyBird/Systems/ScrollingSystem.cs
+++ NezzyBird/Systems/ScrollingSystem.cs
@@ -1,4 +1,5 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Nez;
 using Nez.Sprites;
 using Nez.Systems;
 using NezzyBird.Components;
@@ -31,14 +32,35 @@ namespace NezzyBird.Systems
             var optionalSprite = entity.getComponent<Sprite>();
             var scrolling = entity.getComponent<Scrolling>();
 
+            var movement = determineMovement(scrolling.ScrollDirection, scrolling.Rate);
+
             CollisionResult collisionResult;
-            mover.move(scrolling.Movement, out collisionResult);
+            mover.move(movement, out collisionResult);
 
             var spriteIsNullOrOffscreen = optionalSprite == null || !optionalSprite.isVisible;
 
             if (entity.position.X < 0 && spriteIsNullOrOffscreen)
             {
                 entity.destroy();
+            }
+        }
+
+        private Vector2 determineMovement(
+            ScrollDirection scrollDirection,
+            float rate)
+        {
+            switch (scrollDirection)
+            {
+                case ScrollDirection.Up:
+                    return new Vector2(0, -rate);
+                case ScrollDirection.Left:
+                    return new Vector2(-rate, 0);
+                case ScrollDirection.Down:
+                    return new Vector2(0, rate);
+                case ScrollDirection.Right:
+                    return new Vector2(rate, 0);
+                default:
+                    throw new System.NotImplementedException();
             }
         }
 
