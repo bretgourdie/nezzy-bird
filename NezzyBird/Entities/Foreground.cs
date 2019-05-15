d1dc1fbbf2bdccf310b15d628622afac510a5be2
Bret Gourdie <BretGourdie@gmail.com>
2019-05-15 12:50:46 -0400

    Use correct starting position

--- NezzyBird/Entities/Foreground.cs
+++ NezzyBird/Entities/Foreground.cs
@@ -12,20 +12,26 @@ namespace NezzyBird.Entities
         public Foreground(TextureAtlas textureAtlas)
         {
             var foregroundSubtexture = textureAtlas.getSubtexture("Foreground");
+
             var sprite = new Sprite(foregroundSubtexture);
             sprite.renderLayer = GameConstants.RenderingLevels.Foreground;
             addComponent(sprite);
+
             addComponent(new EndsGameOnCollision());
             addComponent(new BoxCollider() { isTrigger = true });
             addComponent(new Mover());
             addComponent(new Scrolling(ScrollDirection.Left, GameConstants.PIPE_SCROLL_SPEED));
-            addComponent(new ParallaxScrolling());
 
-            this.scale = GameConstants.GetGameScale();
-            this.position = new Vector2(
-                GameConstants.SOURCE_SCREEN_WIDTH / 2,
+            var originalPosition = new Vector2(
+                GameConstants.SOURCE_SCREEN_WIDTH,
                 GameConstants.SOURCE_SCREEN_HEIGHT - sprite.height / 8)
                 * GameConstants.GetGameScale();
+            addComponent(new ParallaxScrolling(originalPosition));
+
+            this.position = originalPosition;
+
+            this.scale = GameConstants.GetGameScale();
+
         }
     }
 }
