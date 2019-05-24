71a21fc865b64449fac5dd9a45a7fb03117637b0
Bret Gourdie <BretGourdie@gmail.com>
2019-05-24 13:46:55 -0400

    Scene-centric approach to screen flash

--- NezzyBird/Components/ScreenFlash.cs
+++ NezzyBird/Components/ScreenFlash.cs
@@ -2,13 +2,17 @@
 
 namespace NezzyBird.Components
 {
-    public class ScreenFlash : Component
+    public class ScreenFlash : SceneComponent
     {
         public bool IsFinished { get; private set; }
         public bool HasStarted { get; private set; }
 
         public const float TotalDuration = 2f;
         public float TimePassed { get; private set; }
+
+        public const float ToWhite = TotalDuration / 2f;
+
+        public const float ToClear = TotalDuration / 2f;
 
         public void AddTime(float deltaTime)
         {
---
+++ NezzyBird/Systems/ScreenFlashSystem.cs
@@ -0,0 +1,18 @@
+﻿using Nez;
+using NezzyBird.Components;
+
+namespace NezzyBird.Systems
+{
+    public class ScreenFlashSystem : ProcessingSystem
+    {
+        public override void process()
+        {
+            var screenFlash = scene.getSceneComponent<ScreenFlash>();
+
+            if (screenFlash == null)
+            {
+                return;
+            }
+        }
+    }
+}
