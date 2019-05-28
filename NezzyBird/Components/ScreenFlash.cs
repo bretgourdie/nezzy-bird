850c05d592f38f1874aa603192da01773fe6b633
Bret Gourdie <BretGourdie@gmail.com>
2019-05-28 12:53:27 -0400

    Going for a state pattern

--- NezzyBird/Components/ScreenFlash.cs
+++ NezzyBird/Components/ScreenFlash.cs
@@ -1,22 +1,62 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
 
 namespace NezzyBird.Components
 {
     public class ScreenFlash : SceneComponent
     {
-        public bool IsFinished { get; private set; }
-        public bool HasStarted { get; private set; }
-
         public const float TotalDuration = 2f;
+
         public float TimePassed { get; private set; }
 
-        public const float ToWhite = TotalDuration / 2f;
+        public Texture2D Overlay { get; private set; }
 
-        public const float ToClear = TotalDuration / 2f;
+        private IScreenFlashState _state { get; set; }
+
+        public ScreenFlash()
+        {
+            Overlay = new Texture2D(
+                Core.graphicsDevice,
+                GameConstants.SCREEN_WIDTH,
+                GameConstants.SCREEN_HEIGHT);
+
+            var clearWhite = Color.White;
+            clearWhite.A = 0;
+
+            Overlay.SetData(new[] { clearWhite });
+
+            _state = new RaisingInOpacity();
+        }
+
+        public void HandleState()
+        {
+            _state = _state.Handle();
+        }
 
         public void AddTime(float deltaTime)
         {
             TimePassed += deltaTime;
+        }
+        private interface IScreenFlashState
+        {
+            IScreenFlashState Handle();
+        }
+
+        private class RaisingInOpacity : IScreenFlashState
+        {
+            public IScreenFlashState Handle()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private class LoweringInOpacity : IScreenFlashState
+        {
+            public IScreenFlashState Handle()
+            {
+                throw new System.NotImplementedException();
+            }
         }
     }
 }
