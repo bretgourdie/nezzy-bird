5d60dc19e81525d569c381f8cffc11e0856230c9
Bret Gourdie <BretGourdie@gmail.com>
2019-06-13 13:30:52 -0400

    Trying out SpriteAlphaTestEffect? Not sure

--- NezzyBird/Systems/FadeSystem.cs
+++ NezzyBird/Systems/FadeSystem.cs
@@ -6,25 +6,38 @@ namespace NezzyBird.Systems
 {
     public class FadeSystem : EntityProcessingSystem
     {
+        public readonly SpriteAlphaTestEffect _spriteAlphaTestEffect;
         public FadeSystem() : base(
             new Matcher().all(
                 typeof(Fades)
             ))
-        { }
+        {
+            _spriteAlphaTestEffect = Core.content.loadNezEffect<SpriteAlphaTestEffect>();
+        }
 
         public override void process(Entity entity)
         {
             var fades = entity.getComponent<Fades>();
             var sprite = entity.getComponent<Sprite>();
 
+            if (sprite.material.effect == null)
+            {
+                var localEffect = _spriteAlphaTestEffect.Clone();
+                sprite.material.effect = localEffect;
+            }
+
             fades.update(Time.deltaTime);
 
             var startingOpacity = _getStartingOpacity(fades.FadeDirection);
             var endingOpacity = _getEndingOpacity(fades.FadeDirection);
+
             var totalTime = fades.TotalTime;
             var timeElapsed = fades.TimeElapsed;
 
             var opacity = Mathf.lerp(startingOpacity, endingOpacity, timeElapsed / totalTime);
+
+            var alphaEffect = sprite.material.effect as SpriteAlphaTestEffect;
+            alphaEffect.referenceAlpha = opacity;
         }
 
         private float _getEndingOpacity(FadeDirection fadeDirection)
