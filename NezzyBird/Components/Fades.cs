ba26c74f7209b1b75e998aeab453c7e88d24a45b
Bret Gourdie <BretGourdie@gmail.com>
2019-06-13 13:30:41 -0400

    Fades should not track opacity

--- NezzyBird/Components/Fades.cs
+++ NezzyBird/Components/Fades.cs
@@ -4,13 +4,9 @@ namespace NezzyBird.Components
 {
     public class Fades : Component
     {
-        public float Opacity { get; set; }
-
         public float TotalTime { get; private set; }
 
         public float TimeElapsed { get; private set; }
-
-        public bool IsFadingOut { get; private set; }
 
         public FadeDirection FadeDirection { get; private set; }
 
@@ -18,15 +14,8 @@ namespace NezzyBird.Components
             FadeDirection fadeDirection,
             float totalTime)
         {
-            Opacity = 1f;
             TotalTime = totalTime;
-            IsFadingOut = false;
             FadeDirection = fadeDirection;
-        }
-
-        public void ToggleFadeOut()
-        {
-            IsFadingOut = !IsFadingOut;
         }
 
         public void update(float deltaTime)
