47be19a4210c1fdd31102542dd0c5a165f15293f
Bret Gourdie <BretGourdie@gmail.com>
2019-06-20 17:52:28 -0400

    Initial skeleton of FirstJumpSystem

---
+++ NezzyBird/Systems/FirstJumpSystem.cs
@@ -0,0 +1,24 @@
+﻿using Nez;
+using Nez.Systems;
+
+namespace NezzyBird.Systems
+{
+    public class FirstJumpSystem : PassiveSystem
+    {
+        private bool _sawFirstJump = false;
+        private readonly Emitter<NezzyEvents> _emitter;
+
+        public FirstJumpSystem(
+            Emitter<NezzyEvents> emitter)
+        {
+            _emitter = emitter;
+            _emitter.addObserver(NezzyEvents.BirdJumped, _handleFirstJump);
+        }
+
+        private void _handleFirstJump()
+        {
+            _emitter.removeObserver(NezzyEvents.BirdJumped, _handleFirstJump);
+            _sawFirstJump = true;
+        }
+    }
+}
