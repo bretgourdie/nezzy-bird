9e0011933e7bb19bdc68c23de94d9e477cfe8e0a
Bret Gourdie <BretGourdie@gmail.com>
2019-06-29 21:42:26 -0400

    Consolidate sprite logic to common object

--- NezzyBird/Systems/ScoreDisplaySystem.cs
+++ NezzyBird/Systems/ScoreDisplaySystem.cs
@@ -1,24 +1,25 @@
 ﻿using Nez;
-using Nez.Sprites;
 using Nez.Systems;
 using Nez.TextureAtlases;
 using NezzyBird.Components;
-using NezzyBird.Entities;
 
 namespace NezzyBird.Systems
 {
     public class ScoreDisplaySystem : EntityProcessingSystem
     {
-        private readonly TextureAtlas _textureAtlas;
         private bool _birdDied;
+        private readonly ScoreSpriteHandler _scoreSpriteHandler;
 
         public ScoreDisplaySystem(
             Emitter<NezzyEvents> emitter,
             TextureAtlas textureAtlas) : base(
             new Matcher().all(typeof(DisplaysNumber)))
         {
-            _textureAtlas = textureAtlas;
             emitter.addObserver(NezzyEvents.BirdDied, () => _birdDied = true);
+            _scoreSpriteHandler =
+                new ScoreSpriteHandler(
+                    ScoreSpriteHandler.SpriteSize.Large,
+                    textureAtlas);
         }
 
         public override void process(Entity entity)
@@ -36,32 +37,7 @@ namespace NezzyBird.Systems
                 return;
             }
 
-            displaysNumber.HandleUpdatedNumber();
-
-            var score = displaysNumber.Number;
-
-            var strScore = score.ToString();
-
-            var sampleSprite = _textureAtlas.getSubtexture("l0");
-            var rectangle = sampleSprite.sourceRect;
-            var spriteWidth = rectangle.Width;
-
-            while (displaysNumber.NumberSpriteHoldersCount < strScore.Length)
-            {
-                var newNumber = new Number(displaysNumber.NumberSpriteHoldersCount, spriteWidth);
-                displaysNumber.AddNumberSpriteHolder(newNumber);
-                scene.addEntity(newNumber);
-            }
-
-            for (int ii = 0; ii < strScore.Length; ii++)
-            {
-                var digit = strScore[ii];
-                var digitSubtexture = _textureAtlas.getSubtexture($"l{digit}");
-                var sprite = new Sprite(digitSubtexture);
-
-                var reversedIndex = strScore.Length - ii - 1;
-                displaysNumber.SetNumberSprite(reversedIndex, sprite);
-            }
+            _scoreSpriteHandler.HandleSprite(entity);
         }
     }
 }
---
+++ NezzyBird/Systems/ScoreSpriteHandler.cs
@@ -0,0 +1,76 @@
+﻿using Nez;
+using Nez.Sprites;
+using Nez.TextureAtlases;
+using NezzyBird.Components;
+using NezzyBird.Entities;
+
+namespace NezzyBird.Systems
+{
+    public class ScoreSpriteHandler
+    {
+        private readonly TextureAtlas _textureAtlas;
+
+        private readonly SpriteSize _spriteSize;
+
+        public ScoreSpriteHandler(
+            SpriteSize spriteSize,
+            TextureAtlas textureAtlas)
+        {
+            _spriteSize = spriteSize;
+            _textureAtlas = textureAtlas;
+        }
+
+        public void HandleSprite(Entity entity)
+        {
+            var displaysNumber = entity.getComponent<DisplaysNumber>();
+
+            displaysNumber.HandleUpdatedNumber();
+
+            var score = displaysNumber.Number;
+
+            var strScore = score.ToString();
+
+            var spriteSizePrefix = getSpriteSizePrefix(_spriteSize);
+
+            var sampleSprite = _textureAtlas.getSubtexture($"{spriteSizePrefix}0");
+            var rectangle = sampleSprite.sourceRect;
+            var spriteWidth = rectangle.Width;
+
+            while (displaysNumber.NumberSpriteHoldersCount < strScore.Length)
+            {
+                var newNumber = new Number(displaysNumber.NumberSpriteHoldersCount, spriteWidth);
+                displaysNumber.AddNumberSpriteHolder(newNumber);
+                entity.scene.addEntity(newNumber);
+            }
+
+            for (int ii = 0; ii < strScore.Length; ii++)
+            {
+                var digit = strScore[ii];
+                var digitSubtexture = _textureAtlas.getSubtexture($"{spriteSizePrefix}{digit}");
+                var sprite = new Sprite(digitSubtexture);
+
+                var reversedIndex = strScore.Length - ii - 1;
+                displaysNumber.SetNumberSprite(reversedIndex, sprite);
+            }
+        }
+
+        private string getSpriteSizePrefix(SpriteSize spriteSize)
+        {
+            switch (spriteSize)
+            {
+                case SpriteSize.Large:
+                    return "l";
+                case SpriteSize.Small:
+                    return "s";
+                default:
+                    throw new System.NotImplementedException();
+            }
+        }
+
+        public enum SpriteSize
+        {
+            Large,
+            Small
+        }
+    }
+}
